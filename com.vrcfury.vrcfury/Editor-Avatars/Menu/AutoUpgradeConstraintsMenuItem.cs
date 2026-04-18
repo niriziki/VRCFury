8de60e7015d95eb253e99cb99cd6516a829f76d1
niriziki <web+10@narazaka.net>
2026-04-18 17:16:44 +0900

    feat(spsndmf): remove AutoUpgradeConstraintsMenuItem menu registration
    A-066: removes [MenuItem] attribute on the non-SPS "auto-upgrade constraints" toggle.
    Get() remains callable; the menu entry no longer appears.

--- com.vrcfury.vrcfury/Editor-Avatars/Menu/AutoUpgradeConstraintsMenuItem.cs
+++ com.vrcfury.vrcfury/Editor-Avatars/Menu/AutoUpgradeConstraintsMenuItem.cs
@@ -17,7 +17,8 @@ namespace VF.Menu {
             UnityEditor.Menu.SetChecked(MenuItems.autoUpgradeConstraints, Get());
         }
 
-        [MenuItem(MenuItems.autoUpgradeConstraints, priority = MenuItems.autoUpgradeConstraintsPriority)]
+        // SPS-NDMF: removed menu registration (non-SPS utility)
+        // [MenuItem(MenuItems.autoUpgradeConstraints, priority = MenuItems.autoUpgradeConstraintsPriority)]
         private static void Click() {
             if (Get()) {
                 var ok = DialogUtils.DisplayDialog(
